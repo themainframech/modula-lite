cccaeb4ef205c72a04522117e383682688efc43d
giucu91 <george.ciobanu91@gmail.com>
2018-07-03 15:07:10 +0300

    #31

--- includes/public/class-modula-shortcode.php
+++ includes/public/class-modula-shortcode.php
@@ -87,7 +87,7 @@ class Modula_Shortcode {
 			case "lightbox2":
 				wp_enqueue_style( 'lightbox2_stylesheet' );
 				wp_enqueue_script( 'lightbox2_script' );
-				wp_add_inline_script( 'lightbox2_script', 'jQuery(document).ready(function(){lightbox.option({albumLabel: "' . esc_html__( 'Image %1 of %2', 'modula-gallery' ) . '"});});' );
+				wp_add_inline_script( 'lightbox2_script', 'jQuery(document).ready(function(){lightbox.option({albumLabel: "' . esc_html__( 'Image %1 of %2', 'modula-gallery' ) . '",wrapAround: true});});' );
 				break;
 			default:
 				do_action( 'modula_lighbox_shortcode', $settings['lightbox'] );
