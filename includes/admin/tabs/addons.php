3f14b9bdecdbe876d7a95c1057c089b21f1b6bd0
alexjpanagis <alexjpanagis@gmail.com>
2018-11-29 18:20:38 +0400

    Update addons.php

--- includes/admin/tabs/addons.php
+++ includes/admin/tabs/addons.php
@@ -1,6 +1,6 @@
 <div class="wrap">
 	<h1 class="wp-heading-inline"><?php _e( 'Modula Addons', 'modula-lite' ) ?></h1>
 	<a href="#" id="modula-force-reload" class="page-title-action"><?php _e( 'Reload Addons', 'modula-lite' ) ?></a>
-	<p><?php _e( 'Addons extend the functionality of Modula Gallery. You can automatically install an addon from this page if avaible.', 'modula-lite' ) ?></p>
+	<p><?php _e( 'Addons are used to extend the core functionality of Modula. You can automatically install an addon from this page if available.', 'modula-lite' ) ?></p>
 </div>
 <div class="modula-addons-error"></div>
